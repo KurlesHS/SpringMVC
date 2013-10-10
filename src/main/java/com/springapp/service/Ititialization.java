fd570fb81422d18680aefa3aea9ebc751a015aaa
KurlesHS <kahshs@gmail.com>
2013-10-10 10:29:06 +0400

    First commit

---
+++ src/main/java/com/springapp/service/Ititialization.java
@@ -0,0 +1,14 @@
+package com.springapp.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Admin
+ * Date: 07.10.13
+ * Time: 16:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class Ititialization {
+    public void start() {
+        System.out.println("Yep!!! Initialize service here");
+    }
+}
